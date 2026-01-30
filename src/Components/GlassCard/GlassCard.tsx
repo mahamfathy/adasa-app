9a4e9014aa5a7b462d22a9ff6a55298bf11f0727
Maha <mahafathyh@gmail.com>
2026-01-30 20:26:50 +0200

    create glasscard file

--- src/Components/GlassCard/GlassCard.tsx
+++ src/Components/GlassCard/GlassCard.tsx
@@ -12,7 +12,7 @@ export default function GlassCard({ icon, number, pargraph }: GlassCardProps) {
         className="glass-card p-4 hover:scale-105 transition-transform duration-300"
         style={{ animationDelay: "0ms" }}
       >
-        <span>{icon}</span>
+        <div>{icon}</div>
         <p className="text-2xl md:text-3xl font-bold gradient-text">{number}</p>
         <p className="text-neutral-500 text-sm">{pargraph}</p>
       </div>
