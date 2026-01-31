702ccced19da918561b5de682ad9ed87a7aa60a8
Maha <mahafathyh@gmail.com>
2026-01-31 20:05:44 +0200

    add main section in blog page

--- src/Pages/Blog/Blog.tsx
+++ src/Pages/Blog/Blog.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import MainSection from "../../Components/MainSection/MainSection";
 
 export default function Blog() {
   return (
-    <div>
-      Blog
+    <>
+      <MainSection
+        sectionLabel="مدونتنا"
+        heading="استكشف مقالاتنا"
+        paragraph="اكتشف الدروس والرؤى وأفضل الممارسات للتطوير الحديث"
+      />
       <Outlet />
-    </div>
+    </>
   );
 }
