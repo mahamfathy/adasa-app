80e0e72f93b7d44edbdbd4f8f3ffe272b366dc4b
Maha <mahafathyh@gmail.com>
2026-02-04 15:57:32 +0200

    add getcategoryicon to categoriessection

--- src/Components/CategoriesSection.tsx/CategoriesSection.tsx
+++ src/Components/CategoriesSection.tsx/CategoriesSection.tsx
@@ -1,12 +1,6 @@
-import {
-  FaCamera,
-  FaMountainSun,
-  FaSliders,
-  FaSun,
-  FaUser,
-} from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import type { Data } from "../../Interfaces/data.interface";
+import getCategoryIcon from "../../Utils/getCategoryIcon";
 import SectionLabel from "../SectionLabel/SectionLabel";
 
 export default function CategoriesSection({
@@ -14,29 +8,6 @@ export default function CategoriesSection({
 }: {
   data: Data["categories"];
 }) {
-  const getCategoryIcon = (name: string) => {
-    if (name === "إضاءة") {
-      return (
-        <FaSun className="text-xl text-orange-500 group-hover:text-white" />
-      );
-    } else if (name === "مناظر طبيعية") {
-      return (
-        <FaMountainSun className="text-xl text-orange-500 group-hover:text-white" />
-      );
-    } else if (name === "معدات") {
-      return (
-        <FaSliders className="text-xl text-orange-500 group-hover:text-white" />
-      );
-    } else if (name === "بورتريه") {
-      return (
-        <FaUser className="text-xl text-orange-500 group-hover:text-white" />
-      );
-    } else {
-      return (
-        <FaCamera className="text-xl text-orange-500 group-hover:text-white" />
-      );
-    }
-  };
   return (
     <>
       <section className="py-24 bg-[#111111] relative border-y border-[#262626]">
