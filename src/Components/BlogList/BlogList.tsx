fdef7b109592b2a45d5687b1083be3629eed02e8
Maha <mahafathyh@gmail.com>
2026-02-04 15:58:36 +0200

    edits

--- src/Components/BlogList/BlogList.tsx
+++ src/Components/BlogList/BlogList.tsx
@@ -1,3 +1,5 @@
-export default function BlogList() {
+import type { Data } from "../../Interfaces/data.interface";
+
+export default function BlogList({ data }: { data: Data }) {
   return <div>BlogList</div>;
 }
