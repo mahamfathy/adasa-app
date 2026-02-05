23427a5705706a83f16ebca9872d329c4d84abad
Maha <mahafathyh@gmail.com>
2026-02-05 12:02:10 +0200

    add routes needed

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ import BlogList from "./Components/BlogList/BlogList";
 import Layout from "./Components/Layout/Layout";
 import MainSection from "./Components/MainSection/MainSection";
 import type { Data } from "./Interfaces/data.interface";
+import AboutUs from "./Pages/AboutUs/AboutUs";
 import Blog from "./Pages/Blog/Blog";
 import BlogDetails from "./Pages/BlogDetails/BlogDetails";
 import Home from "./Pages/Home/Home";
 import NotFound from "./Pages/NotFound/NotFound";
+import Privacy from "./Pages/Privacy/Privacy";
+import Terms from "./Pages/Terms/Terms";
 function App() {
   const [data] = useState<Data>(blogData);
   const routes = createBrowserRouter([
@@ -41,7 +44,9 @@ function App() {
             },
           ],
         },
-        { path: "/about", element: <div>من نحن</div> },
+        { path: "/about", element: <AboutUs /> },
+        { path: "/terms", element: <Terms /> },
+        { path: "/privacy", element: <Privacy /> },
         { path: "*", element: <NotFound /> },
       ],
     },
