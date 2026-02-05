b0f9c4252b849845c787cf46a20ac196f0fe0a29
Maha <mahafathyh@gmail.com>
2026-02-05 13:51:18 +0200

    add main section and valuessection to aboutus

--- src/Pages/AboutUs/AboutUs.tsx
+++ src/Pages/AboutUs/AboutUs.tsx
@@ -1,3 +1,49 @@
+import { FaBookOpen, FaNewspaper, FaPenNib, FaUsers } from "react-icons/fa6";
+import data from "../../../data.json";
+import GlassCard from "../../Components/GlassCard/GlassCard";
+import MainSection from "../../Components/MainSection/MainSection";
+import ValuesSection from "../../Components/ValuesSection/ValuesSection";
 export default function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <>
+      <MainSection
+        heading="مهمتنا هي الإعلام والإلهام"
+        paragraph={data.siteInfo.description}
+        sectionLabel="من نحن"
+      >
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 max-w-3xl mx-auto">
+          <GlassCard
+            icon={
+              <FaUsers className="mx-auto block text-center text-2xl text-orange-500 mb-1" />
+            }
+            number="+2مليون"
+            pargraph="قارئ شهريا"
+          />
+
+          <GlassCard
+            icon={
+              <FaNewspaper className="mx-auto block text-center text-2xl text-orange-500 mb-1" />
+            }
+            number="+500"
+            pargraph="مقالة منشورة"
+          />
+          <GlassCard
+            icon={
+              <FaPenNib className="mx-auto block text-center text-2xl text-orange-500 mb-1" />
+            }
+            number="+50"
+            pargraph="كاتب خبير"
+          />
+          <GlassCard
+            icon={
+              <FaBookOpen className="mx-auto block text-center text-2xl text-orange-500 mb-1" />
+            }
+            number="+15"
+            pargraph="تصنيف"
+          />
+        </div>
+      </MainSection>
+      <ValuesSection />
+    </>
+  );
 }
